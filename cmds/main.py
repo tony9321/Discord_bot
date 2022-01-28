58324b44d56ea4be6d8fb2032ca90cda49948d2c
Tony Chen <tonychen9321@gmail.com>
2022-01-28 23:25:58 +0800

    y

--- cmds/main.py
+++ cmds/main.py
@@ -10,5 +10,7 @@ class Main(Cog_Extension):
     async def ping(self, ctx):
         await ctx.send(f'{round(self.bot.latency*1000)} (ms)')
 
+           
+
 def setup(bot):
     bot.add_cog(Main(bot))
