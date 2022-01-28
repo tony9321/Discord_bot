1d519d7ce4626de532132bc6d5520fa6861c3d2a
Tony Chen <tonychen9321@gmail.com>
2022-01-28 23:19:37 +0800

    y

--- Testbot.py
+++ Testbot.py
@@ -22,11 +22,25 @@ async def on_member_join(member):
     channel = bot.get_channel(int(jdata["Left_channel"]))
     await channel.send(f'{member} join!')
 
-
 @bot.event
 async def on_member_remove(member):
     channel = bot.get_channel(int(jdata["Left_channel"]))
     await channel.send(f'{member} left!')
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f"cmds.{extension}")
+    await ctx.send(f"Loaded {extension} done!")
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f"cmds.{extension}")
+    await ctx.send(f"Unloaded {extension} done!")
+
+@bot.command()
+async def reload(ctx, extension):
+    bot.reload_extension(f"cmds.{extension}")
+    await ctx.send(f"Reloaded {extension} done!")
 
 for filename in os.listdir("./cmds"):
     if filename.endswith('.py'):
