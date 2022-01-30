108e7c43f86d213514b20d7eda7ec091e6eca75a
Tony Chen <tonychen9321@gmail.com>
2022-01-30 16:33:13 +0800

    y

---
+++ cmds/task.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extension
+import json, asyncio, datetime
+
+class Task(Cog_Extension):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        async def interval():
+            await self.bot.wait_until_ready()
+            self.channel = self.bot.get_channel(936114894359638086)
+            while not self.bot.is_closed():
+                await self.channel.send("我還活著拉!!")
+                await asyncio.sleep(5) # sec
+
+        self.bg_task = self.bot.loop.create_task(interval())        
+
+def setup(bot):
+    bot.add_cog(Task(bot))
